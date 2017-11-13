73f7f8e6fa56ce9757ac841aac67fbad4434d60d
user <user@user90>
2017-11-13 16:57:53 +0900

    mk

--- src/main/java/com/food/webapp/controller/customer/CommentController.java
+++ src/main/java/com/food/webapp/controller/customer/CommentController.java
@@ -13,6 +13,7 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -50,7 +51,7 @@ public class CommentController {
 		
 		return "customer.comment.reg";
 	}
-	
+	@Transactional
 	@RequestMapping(value="comment/{id}", method=RequestMethod.POST)
 	public String reg(@PathVariable("id") int id,
 						Comment comment,
@@ -74,23 +75,24 @@ public class CommentController {
 		         if(!f.mkdirs())
 		            System.out.println("디렉토리를 생성할 수가 없습니다.");
 		      }
-	      cmtImage.setCommentId(id);
-	      cmtImage.setMemberId(loginId);
-	      
-	      for(int i = 0; i<file.length; i++) {
-		      path += File.separator + file[i].getOriginalFilename();
-		      System.out.println(file[i].getOriginalFilename());
-		      cmtImage.setSrc(file[i].getOriginalFilename());
-		      commentDao.insertCmtImage(cmtImage);
-		      File f2 = new File(path); 
-		      file[i].transferTo(f2);
-		      path = ctx.getRealPath(String.format("/resource/customer/restaurant/%d/%d/commentImage", year,id));
-	      }
 	      
 	      comment.setMemberId(loginId);
 	      comment.setRestaurantId(id);
 	      commentDao.insert(comment);
 	      
+	      cmtImage.setCommentId(comment.getId());
+	      cmtImage.setMemberId(loginId);
+	      
+	      for(int i = 0; i<file.length; i++) {
+	    	  path += File.separator + file[i].getOriginalFilename();
+	    	  System.out.println(file[i].getOriginalFilename());
+	    	  cmtImage.setSrc(file[i].getOriginalFilename());
+	    	  commentDao.insertCmtImage(cmtImage);
+	    	  File f2 = new File(path); 
+	    	  file[i].transferTo(f2);
+	    	  path = ctx.getRealPath(String.format("/resource/customer/restaurant/%d/%d/commentImage", year,id));
+	    	  System.out.println(path);
+	      }
 		return "redirect:../restaurant/{id}";
 	}
 	
